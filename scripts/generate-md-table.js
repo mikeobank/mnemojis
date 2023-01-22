34b5c33ec2f7ba333e3f01eec2c0cd636a34996a
Mike O'Bank <mike.obank@pm.me>
2023-01-22 14:22:51 +0100

    Improve text / nomenclature scripts

--- scripts/generate-md-table.js
+++ scripts/generate-md-table.js
@@ -2,7 +2,7 @@
 
 const mnemojis = require("../mnemojis.json")
 
-const headers = ["index", "character", "Unicode", "name (English)", "related"]
+const headers = ["index", "symbol", "Unicode", "name (English)", "ambiguous symbols"]
 const header = `| ${ headers.join(" | ") } |`
 const separator = `| ${ headers.map(() => "---").join(" | ") } |`
 const rows = mnemojis.map(({ name, chars }, index) => `| ${ index } | ${ chars[0] } | U+${ chars[0].codePointAt(0).toString(16).padStart(4, '0') } | ${ name } | ${ chars.slice(1).join(" ") } |`)
